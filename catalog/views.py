a478452fff4c9b2aeb130c7a698a9e522109ad89
wwwru <www.rufat@bk.ru>
2023-11-19 13:07:48 +0300

    start

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(requests):
-    return render(requests, 'catalog/catalog.html')
+def index(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(name, phone, message)
+    return render(request, 'catalog/catalog.html')
 
-def home(requests):
-    return render(requests, 'catalog/home.html')
+def home(request):
+    return render(request, 'catalog/home.html')
+
